e752f81027c3dd465b1e48dc83e7ae5227f5d440
bpoltosi <brunopoltosi@gmail.com>
2026-08-22 15:31:51 -0300

    Adicionar classe Desconto como estratégia abstrata para implementação de diferentes tipos de desconto

---
+++ SistemaControleEstacionamento/src/descontos/Desconto.js
@@ -0,0 +1,37 @@
+/**
+ * Estratégia abstrata de desconto.
+ * Novos tipos devem estender esta classe sem alterar ClienteAvulso.
+ */
+class Desconto {
+  /**
+   * @param {string} identificador
+   */
+  constructor(identificador) {
+    if (new.target === Desconto) {
+      throw new Error('Desconto é abstrata e não pode ser instanciada diretamente');
+    }
+    /** @type {string} */
+    this.identificador = identificador;
+  }
+
+  /**
+   * @param {Array<{ dataHoraEntrada: Date }>} historicoTicketsDaPlaca
+   * @param {Date} dataAtual
+   * @returns {boolean}
+   * @abstract
+   */
+  aplicavel(historicoTicketsDaPlaca, dataAtual) {
+    throw new Error('aplicavel deve ser implementado pela subclasse');
+  }
+
+  /**
+   * @param {number} valorBase
+   * @returns {number}
+   * @abstract
+   */
+  aplicar(valorBase) {
+    throw new Error('aplicar deve ser implementado pela subclasse');
+  }
+}
+
+module.exports = Desconto;
